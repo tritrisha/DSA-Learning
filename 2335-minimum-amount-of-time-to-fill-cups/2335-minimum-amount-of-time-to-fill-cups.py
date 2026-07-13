c8968c4f45e363575e20df226b3c86132951d892
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-07-13 10:53:36 +0530

    Time: 0 ms (100%), Space: 19.4 MB (34.68%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -1,21 +1,17 @@
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
         n=0
-        if len(amount)==amount.count(0):
-            return 0
-
         while len(amount)!=amount.count(0):
             amount.sort()
             while amount[0]==0:
                 amount.pop(0)
-            if amount:
-                if len(amount)!=1:
-                    amount[0]-=1
-                    amount[-1]-=1
-                else:
-                    amount[0]-=1
+            
+            if len(amount)!=1:
+                amount[0]-=1
+                amount[-1]-=1
             else:
-                break
+                amount[0]-=1
+
             n+=1
             
         return n
