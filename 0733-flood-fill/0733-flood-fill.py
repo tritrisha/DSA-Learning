ccb0db0866d0ed560ddb192e0188b5bd7df619d0
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-07-31 03:02:11 +0530

    Time: 3 ms (14.5%), Space: 19.8 MB (16.46%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,40 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        new=image.copy()
+        p=image[sr][sc]
+        def dfs(sr, sc):
+            if new[sr][sc]==color:
+                return 
+            elif new[sr][sc]==p:
+                new[sr][sc]=color
+                if sr-1>=0:
+                    dfs(sr-1, sc)
+
+                if sr+1<len(image):
+                    dfs(sr+1, sc) 
+                    
+                if sc+1<len(image[0]):
+                    dfs(sr, sc+1)
+
+                if sc-1>=0:
+                    dfs(sr, sc-1)
+
+                
+
+        
+        dfs(sr, sc)
+        return new
+
+        
+
+
+
+            
+
+
+
+
+
+        
+
+        
