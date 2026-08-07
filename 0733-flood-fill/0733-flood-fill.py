3b2975973e663cc964066758f3ea0d0cebc9c556
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-07 23:24:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (85.19%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,37 +1,30 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        p=image[sr][sc]
-        def dfs(sr, sc):
-            if image[sr][sc]==color:
-                return 
-            elif image[sr][sc]==p:
-                image[sr][sc]=color
-                if sr-1>=0:
-                    dfs(sr-1, sc)
+        r, c= len(image), len(image[0])
+        q=deque()
+        q.append([sr, sc])
+        direc=[(0, 1), (0, -1), (1, 0), (-1, 0)]
+        ori=image[sr][sc]
+        if ori==color:
+            return image
+        image[sr][sc]=color
+        while q:
+            i, j=q.popleft()
+            for x, y in direc:
+                ni=i+x
+                nj=j+y
+                if ni<0 or nj<0 or ni>=r or nj>=c or image[ni][nj]!=ori:
+                    continue
+                
+                image[ni][nj]=color
+                q.append([ni, nj])
 
-                if sr+1<len(image):
-                    dfs(sr+1, sc) 
-                    
-                if sc+1<len(image[0]):
-                    dfs(sr, sc+1)
-
-                if sc-1>=0:
-                    dfs(sr, sc-1)
-
-        
-        dfs(sr, sc)
         return image
 
         
 
-
+                
 
             
-
-
-
-
-
-        
 
         
