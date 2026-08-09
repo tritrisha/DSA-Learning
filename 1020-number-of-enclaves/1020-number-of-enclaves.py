775800ff3c0936ce05d78589e1b7c4591a31fcc9
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-09 21:02:42 +0530

    Time: 55 ms (86.73%), Space: 21.9 MB (87.25%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -2,44 +2,43 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         r=len(grid)
         c=len(grid[0])
+        q=deque()
+        for i in range(r):
+            if grid[i][0]==1:
+                q.append([i, 0])
 
-        def dfs(i, j):
-            if i<0 or j<0 or i>=r or j>=c:
-                return 
-
-            if grid[i][j]==1:
-                grid[i][j]=0
-                dfs(i+1, j)
-                dfs(i-1, j)
-                dfs(i, j-1)
-                dfs(i, j+1)
-
-            return 
+            if grid[i][c-1]==1:
+                q.append([i, c-1])
 
         for j in range(c):
             if grid[0][j]==1:
-                dfs(0, j)
-
+                q.append([0, j])
+            
             if grid[r-1][j]==1:
-                dfs(r-1, j)
+                q.append([r-1, j])
 
-        
+        d=[0,1,0,-1,0]
+        while q:
+            i, j=q.popleft()
+            grid[i][j]=0
+            for m in range(4):
+                x=i+d[m]
+                y=j+d[m+1]
 
-        for i in range(r):
-            if grid[i][0]==1:
-                dfs(i, 0)
+                if x<0 or y<0 or x>=r or y>=c or grid[x][y]==0:
+                    continue
 
-            if grid[i][c-1]==1:
-                dfs(i, c-1)
+                grid[x][y]=0
+                q.append([x, y])
 
-        cou=0
-        for i in range(r):
-            for j in range(c):
+        count=0
+        for i in range(1, r-1):
+            for j in range(1, c-1):
                 if grid[i][j]==1:
-                    cou+=1
-                    
+                    count+=1
 
-        return cou
 
-        
-        
+        return count
+
+
+
