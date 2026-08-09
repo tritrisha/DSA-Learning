507a1758452d269260ab665bcdd3811e3385de57
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-09 23:15:58 +0530

    Time: 258 ms (29.71%), Space: 21.4 MB (98.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,33 +2,50 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         r=len(grid)
         c=len(grid[0])
-        def dfs(i, j):
-            if i<0 or j<0 or i>=r or j>=c or grid[i][j]=="0":
-                return 
+        q=deque()
+        vis=[[False]*c for _ in range(r)]
+
+        di=[0,1,0,-1,0]
+        def bfs():
+            while q:
+                i, j=q.popleft()
+                vis[i][j]=True
+                for k in range(4):
+                    x=i+di[k]
+                    y=j+di[k+1]
+                    if x<0 or y<0 or x>=r or y>=c or vis[x][y]:
+                        continue
+
+                    if grid[x][y]=="1":
+                        vis[x][y]=True
+                        q.append([x,y])
+
+            return 1
+
+
+        count=0
+        for ri in range(r):
+            for cj in range(c):
+                if grid[ri][cj]=="1" and not vis[ri][cj]:
+                    print(grid[ri][cj])
+                    q.append([ri, cj])
+                    count+=bfs()
+
+
+        return count
+
+
+
 
         
-            grid[i][j]="0"
-
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-            return 
-        
-        cou=0
-        for i in range(r):
-            for j in range(c):
-                if grid[i][j]=="1":
-                    cou+=1
-                    dfs(i, j)
-
-        return cou
-
 
 
         
 
 
+                
 
 
+                    
+            
         
