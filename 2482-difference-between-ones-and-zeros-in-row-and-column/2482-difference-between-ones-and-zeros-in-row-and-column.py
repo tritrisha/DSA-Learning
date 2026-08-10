f68d281ecabe13d98459a985658c11e7b9552a12
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-11 00:42:23 +0530

    Time: 118 ms (81.18%), Space: 41.1 MB (100%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -0,0 +1,25 @@
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        r=len(grid)
+        c=len(grid[0])
+        oneinr=[0]*r
+        oneinc=[0]*c
+
+        for i in range(r):
+            for j in range(c):
+                oneinr[i]+=grid[i][j]
+                oneinc[j]+=grid[i][j]
+
+
+        for i in range(r):
+            for j in range(c):
+                grid[i][j]=oneinr[i]+oneinc[j] - (r-oneinr[i])- (c-oneinc[j])
+
+        return grid
+
+
+        
+
+
+
+        
