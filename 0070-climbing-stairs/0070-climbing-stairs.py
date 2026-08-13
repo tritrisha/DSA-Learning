287ba15fd487ae0394001beb3b7c9c62d1e19fab
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-14 03:50:35 +0530

    Time: 0 ms (100%), Space: 19.2 MB (52.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        arr=[-1]*(n+1)
-        arr[0]=1
-        if n>0:
-            arr[1]=1
-        def fab(n):
-            if n==1 or n==0:
-                return arr[n]
-            if arr[n]==-1:
-                arr[n]=fab(n-1)+fab(n-2)
-        
-            return arr[n]
+        a=0
+        b=1
+        for _ in range(n):
+            a, b= b, a+b
 
-        return fab(n)
+        return b
+
+        
