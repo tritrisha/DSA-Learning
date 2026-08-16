8e71e813696d678396d8194c8b77ff6a79cdf48a
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-16 20:26:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.61%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,16 +1,29 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         mod=1000000007
+        def poww(x, r, mod):
+            x=x%mod
+            if r==1:
+                return x%mod
+            if r==0:
+                return 1
+
+            if r%2==0:
+                return poww((x*x), r//2, mod)
+
+            else:
+                return x*poww(x, r-1, mod)
+
         if n==1:
             return 5
 
         if n%2==0:
             r=n//2
-            return (pow(5, r, mod)*pow(4, r, mod))%mod
+            return (poww(5, r, mod)*poww(4, r, mod))%mod
 
         if n%2!=0:
             r=n//2
-            return (pow(5, r+1, mod)*pow(4, r, mod))%mod
+            return (poww(5, r+1, mod)*poww(4, r, mod))%mod
         
 
         
