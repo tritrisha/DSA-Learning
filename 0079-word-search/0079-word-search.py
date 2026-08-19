c420c57e25452981ebba7396a9eacafe02650a62
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-19 21:17:28 +0530

    Time: 4387 ms (27.58%), Space: 19.6 MB (27.21%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,37 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        r=len(board)
+        c=len(board[0])
+        visa=[[0]*c for _ in range(r)]
+        copy=[[0]*c for _ in range(r)]
+        s=[]
+        def dfs(i, j,k):
+            if i<0 or j<0 or i>=r or j>=c or k>=len(word) or board[i][j]!=word[k] or visa[i][j]==1 :
+                return
+
+            visa[i][j]=1
+            s.append(word[k])
+            dfs(i-1, j, k+1 )
+            dfs(i+1, j, k+1)
+            dfs(i, j-1, k+1 )
+            dfs(i, j+1, k+1)
+            
+            if word=="".join(s):
+                return 
+            else:
+                s.pop(-1)
+                visa[i][j]=0
+               
+                
+
+        for i in range(r):
+            for j in range(c):
+                if board[i][j]==word[0]:
+                    dfs(i, j, 0)
+                    if len(s)==len(word):
+                        return True
+
+        return False
+
+
+        
