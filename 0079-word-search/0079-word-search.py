03c9b3f54fae088671f9023404beee9989d44e24
Trisha <95029165+tritrisha@users.noreply.github.com>
2026-08-22 00:48:36 +0530

    Time: 3469 ms (65.39%), Space: 19.4 MB (58.03%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,35 +2,28 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         r=len(board)
         c=len(board[0])
-        visa=[[0]*c for _ in range(r)]
-        s=[]
-        def dfs(i, j,k):
-            if i<0 or j<0 or i>=r or j>=c or k>=len(word) or board[i][j]!=word[k] or visa[i][j]==1 :
-                return
+        def dfs(i, j , k):
+            if k==len(word):
+                return True
+            if i<0 or j<0 or i>=r or j>=c or board[i][j]!=word[k]:
+                return False
 
-            visa[i][j]=1
-            s.append(word[k])
-            dfs(i-1, j, k+1 )
-            dfs(i+1, j, k+1)
-            dfs(i, j-1, k+1 )
-            dfs(i, j+1, k+1)
-            
-            if word=="".join(s):
-                return 
-            else:
-                s.pop(-1)
-                visa[i][j]=0
-               
-                
+            t=board[i][j]
+            board[i][j]='#'
+            if dfs(i+1, j, k+1) or dfs(i-1, j, k+1) or dfs(i, j+1, k+1) or dfs(i, j-1, k+1):
+                return True
+
+            board[i][j]=t
+            return False
 
         for i in range(r):
             for j in range(c):
-                if board[i][j]==word[0]:
-                    dfs(i, j, 0)
-                    if len(s)==len(word):
-                        return True
+                if board[i][j]==word[0] and dfs(i, j, 0):
+                    return True
 
         return False
+
+
 
 
         
